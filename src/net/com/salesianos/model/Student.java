55af08a697ba2bb29625a18387042c6902a0c8c9
Gabriel <usr4035@salesianos-lacuesta.net>
2025-12-07 18:04:12 +0000

    feat:Se creo los metodos de la clase student

---
+++ src/net/com/salesianos/model/Student.java
@@ -0,0 +1,64 @@
+package net.com.salesianos.model;
+
+public class Student {
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private int añoNacimiento;
+    private String centro;
+
+    public Student(String id, String nombre, String apellidos, int añoNacimiento, String centro) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.añoNacimiento = añoNacimiento;
+        this.centro = centro;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getAñoNacimiento() {
+        return añoNacimiento;
+    }
+
+    public void setAñoNacimiento(int añoNacimiento) {
+        this.añoNacimiento = añoNacimiento;
+    }
+
+    public String getCentro() {
+        return centro;
+    }
+
+    public void setCentro(String centro) {
+        this.centro = centro;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + nombre + " " + apellidos + " (" + añoNacimiento + ") - " + centro;
+
+    }
+
+}
